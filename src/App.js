996b18da8dd36658eabc37ccdfa8c2f4af30c3c1
dariokemenovic <dkemenovic@gmail.com>
2021-01-29 00:39:28 +0100

    Create Pet component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+const Pet = () => {
+	return React.createElement("div", {}, [
+		React.createElement("h1", {}, "Balu"),
+		React.createElement("h2", {}, "Dog"),
+		React.createElement("h3", {}, "Bern Mountain")
+	]);
+};
+
+
 const App = () => {
 	return React.createElement(
 		"div",
