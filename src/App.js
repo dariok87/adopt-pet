e75d81d6f45c0dcf7e5394bd5ef86c4bf90ae3fb
dariokemenovic <dkemenovic@gmail.com>
2021-01-29 14:29:42 +0100

    Pass arguments into Pet component via props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-const Pet = () => {
+const Pet = (props) => {
 	return React.createElement("div", {}, [
-		React.createElement("h1", {}, "Balu"),
-		React.createElement("h2", {}, "Dog"),
-		React.createElement("h2", {}, "Bern")
+		React.createElement("h1", {}, props.name),
+		React.createElement("h2", {}, props.animal),
+		React.createElement("h2", {}, props.breed)
 	]);
 };
 
