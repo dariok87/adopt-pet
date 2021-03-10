7e7c79b3f08188c1ce128784588da95e7ee0875b
dariokemenovic <dkemenovic@gmail.com>
2021-03-10 18:03:22 +0100

    Import SearchParams

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt me!</h1>
-      <Pet name="Balu" animal="Dog" breed="Bern"/>
-      <Pet name="Bella" animal="Cat" breed="British"/>
-      <Pet name="Arny" animal="Rabbit" breed="House"/>
+      <SearchParams />
     </div>
   )
 };
