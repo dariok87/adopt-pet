86fbb587f6f856e1df3abadddf7f54848f4f61d2
dariokemenovic <dkemenovic@gmail.com>
2021-03-10 19:12:43 +0100

    Display pets with async API request

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -5,22 +5,36 @@ import useDropdown from "./useDropdown";
 const SearchParams = () => {
   const [location, updateLocation] = useState("Seattle, WA"); //this is a hook. All hooks begin with "use".
   const [breeds, updateBreeds] = useState([]); // Empty array because we are requesting from API. "I have a dog, give me back all of the dog breeds".
-  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS); // abel, defaultState, options
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS); // label, defaultState, options 
   const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    })
+
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     updateBreeds([]);
     updateBreed("");
 
-    pet.breeds(animal).then(({ breeds }) => {
-      const breedStrings = breeds.map(({ name }) => name);
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedStrings = apiBreeds.map(({ name }) => name);
       updateBreeds(breedStrings);
     }, console.error);
   }, [animal, updateBreed, updateBreeds]); // only run when these things change, not after every single render.
 
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        requestPets()
+      }}>
         <label htmlFor="location">
           Location
           <input 
