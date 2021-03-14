ba14a89f707fa4319b95e9e5766887f32320985f
dariokemenovic <dkemenovic@gmail.com>
2021-03-14 23:58:01 +0100

    Create Results component

---
+++ src/Results.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {pets.length === 0 ? (
+        <h1>No pets found</h1>
+      ) : (
+        pets.map(pet => (
+          <Pet 
+            animal={pets.type}
+            key={pets.id}
+            name={pets.name}
+            breed={pets.breeds.primary}
+            media={pets.photos}
+            location={`${pet.contact.address.city}, ${
+              pet.contact.address.state
+            }`}
+            id={pet.id}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
