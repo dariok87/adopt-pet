68ede7d4f9ee240c46b2808ceb52308eee707a5f
dariokemenovic <dkemenovic@gmail.com>
2021-03-19 22:12:45 +0100

    Import Reach Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
 
 const App = () => {
