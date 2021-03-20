587c86d1434c66bfcaaf41a6045ebd7e0a880914
dariokemenovic <dkemenovic@gmail.com>
2021-03-20 14:48:42 +0100

    Put Details and SearchParams in Router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 import { render } from "react-dom";
 import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <React.StrictMode>
        <div>
         <h1 id="something-important">Adopt me!</h1>
-        <SearchParams />
+        <Router>
+          <SearchParams />
+          <Details />
+        </Router>
       </div>
     </React.StrictMode>
   )
