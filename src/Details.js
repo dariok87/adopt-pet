4b7a58c6a66b1f028b31e8b1ff325c0f91f78179
dariokemenovic <dkemenovic@gmail.com>
2021-03-20 15:36:07 +0100

    Change component into class component

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,31 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = () => {
-  return <h1>hi there</h1>
-};
+class Details extends React.Component {
+  constructor(props) { // set the loading state
+    super(props);
+
+    this.state = {
+      loading: true
+    };
+  }
+  componentDidMount () { // lifecycle method
+    pet.animal(this.props.id)
+      .then(({ animal }) => {
+        this.setState({
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          description: animal.description,
+          media: animal.photos,
+          breed: animal.breds.primary, 
+          loading: false
+        });
+      }, console.error);
+  }
+  render () {
+    return;
+  }
+}
 
 export default Details;
