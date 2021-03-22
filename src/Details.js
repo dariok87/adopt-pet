c4c14d72ffa78a5aab16d36f92b3959bba86013f
dariokemenovic <dkemenovic@gmail.com>
2021-03-22 11:51:57 +0100

    Render the component

--- src/Details.js
+++ src/Details.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React from "react";
 import pet from "@frontendmasters/pet";
 
@@ -24,7 +25,23 @@ class Details extends React.Component {
       }, console.error);
   }
   render () {
-    return;
+    if (this.state.loading) {
+      return <h1>loading...</h1>
+    }
+
+    const { animal, breed, location, description, name } =
+    this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
   }
 }
 
